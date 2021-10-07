a7b35234bb1b8e77ad37ce051c8d86fce02d1b4b
czz <1972234823@qq.com>
2021-10-07 20:59:32 +0800

    first commit

---
+++ src/main/java/com/jxd/gitTest/GitTest.java
@@ -0,0 +1,12 @@
+package com.jxd.gitTest;
+
+/**
+ * @Author:chenzhunzhun
+ * @Date:2021/10/7
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
+
